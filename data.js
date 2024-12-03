d61a812cf0a927af364f895a4ec88acab6f6d975
CesareMazzilli <cesaremazzilli98@gmail.com>
2024-12-03 18:06:47 +0100

    feat: done

---
+++ data.js
@@ -0,0 +1,34 @@
+const posts = [
+    {
+      title: 'Introduzione a JavaScript',
+      content: 'JavaScript è uno dei linguaggi più popolari al mondo...',
+      image: '/static/images/js-intro.jpg',
+      tags: ['JavaScript', 'programmazione', 'web']
+    },
+    {
+      title: 'Guida a Express.js',
+      content: 'Express è un framework minimale per Node.js...',
+      image: '/static/images/express-guide.jpg',
+      tags: ['Express', 'Node.js', 'backend']
+    },
+    {
+      title: 'Cos’è l’HTML?',
+      content: 'HTML è il linguaggio di markup usato per creare pagine web...',
+      image: '/static/images/html-intro.jpg',
+      tags: ['HTML', 'frontend', 'web']
+    },
+    {
+      title: 'CSS: Stile per il web',
+      content: 'CSS aggiunge stile e design alle pagine HTML...',
+      image: '/static/images/css-design.jpg',
+      tags: ['CSS', 'design', 'frontend']
+    },
+    {
+      title: 'Version Control con Git',
+      content: 'Git è uno strumento indispensabile per ogni sviluppatore...',
+      image: '/static/images/git-version.jpg',
+      tags: ['Git', 'version-control', 'collaborazione']
+    },
+  ];
+
+  module.exports = posts;
